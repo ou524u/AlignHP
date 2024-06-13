83a0b0cf4b13edaefc18c4735886049800a4e1b4
U-DESKTOP-L1GAEOA\ou524u <ou524u@stu.pku.edu.cn>
2024-06-13 20:22:52 +0800

    critic

--- prepare/prepare_dataset.sh
+++ prepare/prepare_dataset.sh
@@ -1,13 +1,19 @@
+cd MotionCritic
 mkdir datasets
+cd datasets
 gdown https://drive.google.com/uc?id=1H5MAPBIAygGV5HSa2yIftWDdGq4fPEXB
 unzip mlists.zip
 rm -f mlists.zip
 
-
+cd ..
+mkdir marked
 cd marked
 # download our organized annotations.
 gdown https://drive.google.com/uc?id=1Lgg_ccVvAfxvH0UF-w3Z5OI2rvdFy53V
 unzip annotations-organized.zip
 rm -f annotations-organized.zip
 
+
+# download full annotations, in case you need
+gdown https://drive.google.com/uc?id=1TpZ0nVvx2c84rYGmHsdLgNbu8gBwLGkA
 
---
+++ prepare/prepare_model.sh
@@ -0,0 +1,8 @@
+cd MotionCritic
+mkdir pretrained
+cd pretrained
+gdown https://drive.google.com/uc?id=1vifu1vktjCWDpyPpzGPugzHNalhsaMpq
+
+
+
+
