251d508285780adec5a7f59124499316675aa5cf
U-DESKTOP-L1GAEOA\ou524u <ou524u@stu.pku.edu.cn>
2024-06-13 15:33:36 +0800

    yagate

---
+++ prepare/prepare_data.sh
@@ -0,0 +1,13 @@
+mkdir datasets
+gdown https://drive.google.com/uc?id=1H5MAPBIAygGV5HSa2yIftWDdGq4fPEXB
+unzip mlists.zip
+rm -f mlists.zip
+
+
+cd marked
+# download our organized annotations.
+gdown https://drive.google.com/uc?id=1Lgg_ccVvAfxvH0UF-w3Z5OI2rvdFy53V
+unzip annotations-organized.zip
+rm -f annotations-organized.zip
+
+
